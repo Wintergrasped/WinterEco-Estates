2117049e23ee3e2e2459d6ee8da3b5084e4a7504
Wintergrasped <Wintergrasped@users.noreply.github.com>
2017-09-24 17:09:32 -0700

    Added Firework Effects
    - Added Configurable Firework Effects

--- src/net/wintereco/Wintergrasped/Main.java
+++ src/net/wintereco/Wintergrasped/Main.java
@@ -10,20 +10,27 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -302,6 +309,14 @@ public class Main extends JavaPlugin implements Listener {
 			    					   PropertysForSale.remove(args[1]);
 			    					   sender.sendMessage(TAG+ChatColor.GREEN+"You bought "+args[1]+" for "+this.getConfig().getInt("Propertys."+args[1]+".Price"));
 			    					   
+			    					   
+			    					   if (conf.getBoolean("FireworkEffect")) {
+			    						   spawnFirework(P);
+			    						   spawnFirework(P);
+			    						   spawnFirework(P);
+			    						   spawnFirework(P);
+			    					   }
+			    					   
 			    					   DefaultDomain OWNS = new DefaultDomain();
 			    						OWNS.removeAll();
 			    						OWNS.addPlayer(P.getUniqueId());
@@ -392,6 +407,12 @@ public class Main extends JavaPlugin implements Listener {
 			if (args[1].equalsIgnoreCase("city")) {
 				if (econ.has(Bukkit.getPlayer(sender.getName()), FoundingCity)) {
 					Citys.add(args[2]);
+					if (conf.getBoolean("FireworkEffect")) {
+						   spawnFirework(P);
+						   spawnFirework(P);
+						   spawnFirework(P);
+						   spawnFirework(P);
+					   }
 					sender.sendMessage(TAG+ChatColor.GREEN+" You have founded City of "+args[2]);
 					sender.sendMessage(TAG+ChatColor.GREEN+" You can set your Tax with /weco city settax <City Name> <Ammount> (Ammount Between 0 - 0.30)");
 					this.getConfig().set("Citys."+args[2]+".Mayor", Bukkit.getPlayer(sender.getName()).getUniqueId().toString());
@@ -481,6 +502,12 @@ public class Main extends JavaPlugin implements Listener {
 						   LDS.add(IDL);
 						   conf.set("LoanList", LDS);
 						   
+					   }
+					   if (conf.getBoolean("FireworkEffect")) {
+						   spawnFirework(P);
+						   spawnFirework(P);
+						   spawnFirework(P);
+						   spawnFirework(P);
 					   }
 					   
 					
@@ -643,6 +670,102 @@ public class Main extends JavaPlugin implements Listener {
 	 ****END****
 	 ***********/
 	
+	
+	public void spawnFirework(Player p) {
+		//Spawn the Firework, get the FireworkMeta.
+        Firework fw = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
+        FireworkMeta fwm = fw.getFireworkMeta();
+       
+        //Our random generator
+        Random r = new Random();   
+
+        //Get the type
+        int rt = r.nextInt(5) + 1;
+        Type type = Type.BALL;       
+        if (rt == 1) type = Type.BALL;
+        if (rt == 2) type = Type.BALL_LARGE;
+        if (rt == 3) type = Type.BURST;
+        if (rt == 4) type = Type.CREEPER;
+        if (rt == 5) type = Type.STAR;
+       
+        //Get our random colours   
+        int r1i = r.nextInt(17) + 1;
+        int r2i = r.nextInt(17) + 1;
+        Color c1 = getColor(r1i);
+        Color c2 = getColor(r2i);
+       
+        //Create our effect with this
+        FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
+       
+        //Then apply the effect to the meta
+        fwm.addEffect(effect);
+       
+        //Generate some random power and set it
+        int rp = r.nextInt(2) + 1;
+        fwm.setPower(rp);
+       
+        //Then apply this to our rocket
+        fw.setFireworkMeta(fwm);           
+	}
+	
+	private Color getColor(int i) {
+		Color c = null;
+		if(i==1){
+		c=Color.AQUA;
+		}
+		if(i==2){
+		c=Color.BLACK;
+		}
+		if(i==3){
+		c=Color.BLUE;
+		}
+		if(i==4){
+		c=Color.FUCHSIA;
+		}
+		if(i==5){
+		c=Color.GRAY;
+		}
+		if(i==6){
+		c=Color.GREEN;
+		}
+		if(i==7){
+		c=Color.LIME;
+		}
+		if(i==8){
+		c=Color.MAROON;
+		}
+		if(i==9){
+		c=Color.NAVY;
+		}
+		if(i==10){
+		c=Color.OLIVE;
+		}
+		if(i==11){
+		c=Color.ORANGE;
+		}
+		if(i==12){
+		c=Color.PURPLE;
+		}
+		if(i==13){
+		c=Color.RED;
+		}
+		if(i==14){
+		c=Color.SILVER;
+		}
+		if(i==15){
+		c=Color.TEAL;
+		}
+		if(i==16){
+		c=Color.WHITE;
+		}
+		if(i==17){
+		c=Color.YELLOW;
+		}
+		 
+		return c;
+		}
+		 
+
 	
 	public void evictPlayer(Player P, String Property, int Amount) {
 		
